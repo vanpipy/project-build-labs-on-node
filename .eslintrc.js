3f93bd560cd6cb80280e0c168d9f74cb743b1c7c
vanpipy <vanpipy@gmail.com>
2020-10-22 16:52:52 +0800

    chore: add eslint estensions js and ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
+  settings: {
+    "import/extensions": ['.js', '.ts']
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
