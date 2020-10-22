2905895da48726ab3c10cd42c3f9220960d217ea
vanpipy <vanpipy@gmail.com>
2020-10-22 16:06:11 +0800

    chore: replace the img of the text cause nothing file loader now

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Logo</h1>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
