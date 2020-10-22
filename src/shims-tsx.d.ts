86b3a14af275d2947581d08350d02cf8c9362cbd
vanpipy <vanpipy@gmail.com>
2020-10-22 17:52:42 +0800

    fix: correct the result of eslint

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -2,9 +2,9 @@ import Vue, { VNode } from 'vue';
 
 declare global {
   namespace JSX {
-    // tslint:disable no-empty-interface
+    /* eslint-disable */
     interface Element extends VNode {}
-    // tslint:disable no-empty-interface
+    /* eslint-disable */
     interface ElementClass extends Vue {}
     interface IntrinsicElements {
       [elem: string]: any;
