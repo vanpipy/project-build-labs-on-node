10b4854beee768482496e058ea52c75fc62b8654
vanpipy <vanpipy@gmail.com>
2020-10-22 17:02:08 +0800

    feat: add webpack base config

---
+++ configs/webpack.base.config.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { WebpackPluginServe: Serve } = require('webpack-plugin-serve');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: [
+            path.resolve(__dirname, '../src/main.ts'),
+            'webpack-plugin-serve/client'
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.(css|less)$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.json', '.vue', '.css', '.less']
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new Serve({
+            host: 'localhost',
+            port: 9090,
+            hmr: true,
+            compress: true,
+            static: path.resolve(__dirname, '../dist')
+        })
+    ],
+    optimization: {
+        runtimeChunk: {
+            name: 'mainfest'
+        },
+        splitChunks: {
+            cacheGroups: {
+                common: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'common',
+                    chunks: 'all',
+                    enforce: true,
+                    priority: 2
+                },
+                default: false
+            }
+        }
+    },
+    watch: true
+}
